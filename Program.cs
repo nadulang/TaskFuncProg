c7ac86cbf12c344e6f9663ed9a0c28001897d4a1
Training Gigaming <gigaming@ggm02s-iMac.local>
2020-03-02 19:23:51 +0700

    satu

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Net.WebSockets;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace TaskFuncProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = "hello world again".Capitalize();
+            Console.WriteLine(hello);
+
+            var hello1 = "hello world again".SnakeCase();
+            Console.WriteLine(hello1);
+            var hello2 = "hello world again".KebabCase();
+            Console.WriteLine(hello2);
+
+            var numbers = new []{1,2,3,4,5,6,6,8,8,6,9};
+            var grades = new[]{87.5, 88.5, 60.5, 90.5, 88.5};
+
+            var mostNumbers = numbers.Mode();
+
+            var mostGrades = grades.Mode();
+
+            var tulisanPanjang = "ini adalah tulisan yang sangat panjang".Trim(8);
+            Console.WriteLine(tulisanPanjang);
+
+            var tulisanPanjang1 = "ini adalah tulisan yang sangat panjang".TrimWords(3);
+            Console.WriteLine(tulisanPanjang1);
+        }
+    }
+
+    public static class IEnumerableExtension
+    {
+        public static string Capitalize(this string values) 
+        {
+                var input = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(values);
+                return input;
+        }
+
+        public static string SnakeCase(this string values) => values.Replace(" ", "_");
+        
+        public static string KebabCase(this string values) => values.Replace(" ", "-");
+        
+        public static string Trim(this string values, int i) => values.Substring(0, i) + "....";
+
+        public static string TrimWords(this string values, int i) => (string.Join(" ", values.Split(' ').ToList().GetRange(0, i)));
+    
+        public static T Mode<T>(this IEnumerable<T> values)
+        {
+            return (values.GroupBy(e => e).OrderByDescending(e => e.Count()).Select(e => e.Key)).First();
+        }
+    }
+}
+
+
